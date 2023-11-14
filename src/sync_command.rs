d13f821d283a857458d047745a11f36d1eb53840
Christoph Bartschat <christoph@barts.chat>
2023-11-13 18:52:55 -0800

    sync

--- src/sync_command.rs
+++ src/sync_command.rs
@@ -1,23 +1,63 @@
-use git2::{Error, Repository};
+use git2::{Error, RebaseOptions, Repository};
 use log::debug;
 
 use crate::{cli::SyncArgs, ctx::Ctx};
 
-fn sync_branch(repo: &mut Repository, branch: &str, base: &str) -> Result<(), Error> {
-    let target_branch = repo.find_branch(&branch, git2::BranchType::Local)?;
-    let start_commit = target_branch.into_reference().peel_to_commit()?;
-    let base_branch = repo.find_branch(&base, git2::BranchType::Local)?;
-    let base_commit = base_branch.into_reference().peel_to_commit()?;
+fn sync_branch(repo: &mut Repository, branch: &str) -> Result<(), Error> {
+    let branch_ref = repo
+        .find_branch(&branch, git2::BranchType::Local)?
+        .into_reference();
+    let main_ref = repo
+        .find_branch("main", git2::BranchType::Local)?
+        .into_reference();
 
-    let main_branch = repo.find_branch("main", git2::BranchType::Local)?;
-    let main_commit = main_branch.into_reference().peel_to_commit()?;
-    let branch_id = repo.find_annotated_commit(base_commit.id())?;
-    let onto = repo.find_annotated_commit(start_commit.id())?;
-    let upstream = repo.find_annotated_commit(main_commit.id())?;
+    let upstream_id = repo.reference_to_annotated_commit(&&main_ref)?;
+    let branch_id = repo.reference_to_annotated_commit(&branch_ref)?;
+
+    // let branch_commit = branch_ref.peel_to_commit()?;
+    // let main_commit = main_ref.peel_to_commit()?;
 
     // let fork_point = repo.merge_base(start_commit.id(), base_commit.id())?;
-    let mut rebase = repo.rebase(Some(&branch_id), Some(&upstream), Some(&onto), None)?;
-    let result = rebase.commit(None, &repo.signature()?, None)?;
+
+    debug!("Attempting to rebase...");
+
+    // debug!("main_commit: {:?}", main_commit.id(),);
+
+    // debug!("branch_commit: {:?}", branch_commit.id(),);
+    debug!(
+        "branch: {:?}, ref: {:?}",
+        branch_id.id(),
+        branch_id.refname().unwrap_or("<unset>")
+    );
+
+    debug!(
+        "upstream: {:?}, ref: {:?}",
+        upstream_id.id(),
+        upstream_id.refname().unwrap_or("<unset>")
+    );
+    // debug!("onto: {:?}", onto.id());
+
+    // if branch_id.id() == upstream.id() {
+    //     debug!("No change.");
+    //     return Ok(());
+    // }
+
+    let mut rebase = repo.rebase(
+        Some(&branch_id),
+        Some(&upstream_id),
+        None,
+        Some(&mut RebaseOptions::new().inmemory(true)),
+    )?;
+
+    debug!("Rebase started.");
+
+    while let Some(Ok(operation)) = rebase.next() {
+        debug!("Looking at conflict: {:?}", operation);
+    }
+
+    let result = rebase.finish(Some(&repo.signature()?))?;
+
+    // let result = rebase.commit(None, &repo.signature()?, None)?;
     debug!("rebased and got {:?}", result);
     // repo.branch(&branch, result, true)?;
 
@@ -26,7 +66,7 @@ fn sync_branch(repo: &mut Repository, branch: &str, base: &str) -> Result<(), Er
 
 pub fn sync_command(ctx: &mut Ctx, args: &SyncArgs) -> Result<(), ()> {
     for branch in &args.names {
-        sync_branch(&mut ctx.repo, &branch, "main").map_err(|err| {
+        sync_branch(&mut ctx.repo, &branch).map_err(|err| {
             debug!("Failed to sync {} due to {:?}", branch, err);
             return ();
         })?;
