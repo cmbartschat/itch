c2886a68ac907c0d678235c7ec42ce5d2c260a62
Christoph Bartschat <christoph@barts.chat>
2023-11-29 00:25:42 -0800

    add new files to diff

--- src/diff_command.rs
+++ src/diff_command.rs
@@ -1,4 +1,4 @@
-use git2::{Error, IntoCString};
+use git2::{Delta, DiffOptions, Error, IntoCString};
 use log::debug;
 
 use crate::{base::resolve_base, cli::DiffArgs, ctx::Ctx};
@@ -8,31 +8,50 @@ fn _diff_command(ctx: &Ctx, args: &DiffArgs) -> Result<(), Error> {
     let base_branch = ctx.repo.find_branch(&base, git2::BranchType::Local)?;
     let base_tree = base_branch.into_reference().peel_to_tree()?;
     debug!("{:?}", base_tree);
+
+    let mut options = DiffOptions::new();
+
+    options.include_untracked(true).include_typechange(true);
+
+    let diff_options = Some(&mut options);
+
     let diff = match &args.target {
         Some(branch) => {
             let target_branch = ctx.repo.find_branch(&branch, git2::BranchType::Local)?;
             let target_tree = target_branch.into_reference().peel_to_tree()?;
             ctx.repo
-                .diff_tree_to_tree(Some(&base_tree), Some(&target_tree), None)?
+                .diff_tree_to_tree(Some(&base_tree), Some(&target_tree), diff_options)?
         }
-        _ => ctx.repo.diff_tree_to_workdir(Some(&base_tree), None)?,
+        _ => ctx
+            .repo
+            .diff_tree_to_workdir(Some(&base_tree), diff_options)?,
     };
     diff.print(git2::DiffFormat::Patch, |_, _, line| {
         let origin = line.origin();
         let color_code = match origin {
-            '+' => "\x1b[32m",
-            '-' => "\x1b[31m",
+            '+' => "\x1b[32m+",
+            '-' => "\x1b[31m-",
             _ => "",
         };
 
         print!(
-            "{}{}{}\x1b[0m",
+            "{}{}\x1b[0m",
             color_code,
-            line.origin(),
             line.content().into_c_string().unwrap().to_str().unwrap()
         );
         return true;
     })?;
+
+    diff.deltas().for_each(|delta| match delta.status() {
+        Delta::Untracked => {
+            println!(
+                "\x1b[32mNew file: {}\x1b[0m",
+                delta.new_file().path().unwrap().to_str().unwrap()
+            );
+        }
+        _ => {}
+    });
+
     Ok(())
 }
 
