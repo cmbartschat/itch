b85c9d50e70790e9d8e855d92cea7d9cec65b895
Christoph Bartschat <christoph@barts.chat>
2023-12-29 00:01:52 -0500

    fix sync

--- src/command/sync.rs
+++ src/command/sync.rs
@@ -3,9 +3,99 @@ use std::{
     ops::BitAnd,
 };
 
-use git2::{Error, ErrorCode, Oid, RebaseOperationType, RebaseOptions, Repository};
+use git2::{
+    Error, ErrorCode, Index, IndexConflict, IndexEntry, Oid, RebaseOperationType, RebaseOptions,
+    Repository,
+};
 
-use crate::{cli::SyncArgs, ctx::Ctx, path::bytes2path};
+use crate::{
+    cli::{SaveArgs, SyncArgs},
+    command::save::save_command,
+    consts::TEMP_COMMIT_PREFIX,
+    ctx::Ctx,
+    path::bytes2path,
+    reset::pop_and_reset,
+};
+
+fn yes_or_no(prompt: &str, by_default: Option<bool>) -> bool {
+    print!("{prompt} ");
+
+    match by_default {
+        Some(true) => print!("Y/n "),
+        Some(false) => print!("y/N "),
+        None => print!("y/n "),
+    };
+
+    loop {
+        io::stdout().flush().unwrap();
+        let mut input = String::new();
+        io::stdin().read_line(&mut input).unwrap();
+        match (input.trim(), by_default) {
+            ("y", _) => return true,
+            ("n", _) => return false,
+            ("", Some(r)) => return r,
+            _ => {
+                println!("Unrecognized option. Try again:");
+            }
+        }
+    }
+}
+
+fn entry_without_conflicts(mut entry: IndexEntry) -> IndexEntry {
+    entry.flags = entry.flags.bitand(0x3000_u16.reverse_bits());
+    entry
+}
+
+fn resolve_conflict(index: &mut Index, conflict: IndexConflict) -> Result<(), Error> {
+    match (conflict.their, conflict.our) {
+        (Some(branch_entry), Some(main_entry)) => {
+            let current_path = bytes2path(&branch_entry.path)?;
+            let main_path = bytes2path(&main_entry.path)?;
+
+            let prompt = format!(
+                "{} is conflicted. Keep your changes?",
+                current_path.to_string_lossy(),
+            );
+
+            if yes_or_no(&prompt, None) {
+                index.remove_path(&main_path)?;
+                index.add(&entry_without_conflicts(branch_entry))?;
+            } else {
+                index.remove_path(&current_path)?;
+                index.add(&entry_without_conflicts(main_entry))?;
+            };
+        }
+        // File deleted on main
+        (Some(branch_entry), None) => {
+            let current_path = bytes2path(&branch_entry.path)?;
+            let prompt = format!(
+                "{} was deleted on main. Keep your changes?",
+                current_path.to_string_lossy()
+            );
+            if yes_or_no(&prompt, Some(true)) {
+                index.add(&entry_without_conflicts(branch_entry))?;
+            } else {
+                index.remove_path(&current_path)?;
+            }
+        }
+        // File deleted on branch
+        (None, Some(main_entry)) => {
+            let current_path = bytes2path(&main_entry.path)?;
+            let prompt = format!(
+                "{} was modified on main. Keep your delete?",
+                current_path.to_string_lossy()
+            );
+            if yes_or_no(&prompt, Some(true)) {
+                index.remove_path(&current_path)?;
+            } else {
+                index.add(&entry_without_conflicts(main_entry))?;
+            }
+        }
+        _ => {}
+    };
+
+    Ok(())
+}
 
 fn sync_branch(repo: &Repository, branch_name: &str) -> Result<(), Error> {
     let branch_ref = repo
@@ -25,91 +115,41 @@ fn sync_branch(repo: &Repository, branch_name: &str) -> Result<(), Error> {
         Some(&mut RebaseOptions::new().inmemory(true)),
     )?;
 
-    let mut final_id: Option<Oid> = None;
+    let mut final_id: Oid = upstream_id.id();
 
-    if rebase.len() == 0 {
-        final_id = Some(upstream_id.id());
-    } else {
-        while let Some(Ok(operation)) = rebase.next() {
-            match operation.kind() {
-                Some(RebaseOperationType::Pick) => {
-                    let mut index = rebase.inmemory_index()?;
-                    if index.has_conflicts() {
-                        let conflicts = index.conflicts()?;
-                        println!("\nWe have some conflicts to resolve: {}", conflicts.count());
+    while let Some(Ok(operation)) = rebase.next() {
+        match operation.kind() {
+            Some(RebaseOperationType::Pick) => {
+                let mut index = rebase.inmemory_index()?;
+                if index.has_conflicts() {
+                    let conflicts = index.conflicts()?;
+                    println!("\nWe have some conflicts to resolve: {}", conflicts.count());
 
-                        rebase.inmemory_index()?.conflicts()?.try_for_each(
-                            |conflict| -> Result<(), Error> {
-                                let conflict = conflict?;
-                                let Some(ours) = conflict.our else {
-                                    return Err(Error::from_str(
-                                        "Expected conflict to contain 'our'",
-                                    ));
-                                };
-                                let Some(theirs) = conflict.their else {
-                                    return Err(Error::from_str(
-                                        "Expected conflict to contain 'their'",
-                                    ));
-                                };
-
-                                let current_path = bytes2path(&ours.path)?;
-
-                                if current_path != bytes2path(&theirs.path)? {
-                                    return Err(Error::from_str(
-                                        "Conflict contains two different files.",
-                                    ));
-                                }
-
-                                println!(
-                                    "File: {} is conflicted. Keep yours? y/n ",
-                                    current_path.to_string_lossy()
-                                );
-                                let keep_yours = loop {
-                                    io::stdout().flush().unwrap();
-                                    let mut input = String::new();
-                                    io::stdin().read_line(&mut input).unwrap();
-                                    if input.trim() == "y" {
-                                        break true;
-                                    } else if input.trim() == "n" {
-                                        break false;
-                                    }
-                                    println!("Unknown command. Try again:");
-                                };
-
-                                index.remove_path(&current_path)?;
-
-                                let mut final_entry = if keep_yours { theirs } else { ours };
-
-                                final_entry.flags =
-                                    final_entry.flags.bitand(0x3000_u16.reverse_bits());
-
-                                index.add(&final_entry)?;
-
-                                Ok(())
-                            },
-                        )?;
-                    }
+                    rebase
+                        .inmemory_index()?
+                        .conflicts()?
+                        .try_for_each(|conflict| resolve_conflict(&mut index, conflict?))?;
                 }
-                Some(RebaseOperationType::Fixup) => {
-                    // Ok, whatever
-                    todo!("Handle fixup");
-                }
-                Some(RebaseOperationType::Edit) => {
-                    todo!("Handle edit");
-                }
-                _ => {
-                    todo!("Handle: {:?}", operation);
-                }
-            };
+            }
+            Some(RebaseOperationType::Fixup) => {
+                // Ok, whatever
+                todo!("Handle fixup");
+            }
+            Some(RebaseOperationType::Edit) => {
+                todo!("Handle edit");
+            }
+            _ => {
+                todo!("Handle: {:?}", operation);
+            }
+        };
 
-            match rebase.commit(None, &repo.signature()?, None) {
-                Ok(id) => {
-                    final_id = Some(id);
-                }
-                Err(e) => {
-                    if e.code() != ErrorCode::Applied {
-                        return Err(e);
-                    }
+        match rebase.commit(None, &repo.signature()?, None) {
+            Ok(id) => {
+                final_id = id;
+            }
+            Err(e) => {
+                if e.code() != ErrorCode::Applied {
+                    return Err(e);
                 }
             }
         }
@@ -117,34 +157,40 @@ fn sync_branch(repo: &Repository, branch_name: &str) -> Result<(), Error> {
 
     rebase.finish(Some(&repo.signature()?))?;
 
-    match final_id {
-        Some(id) => {
-            let final_commit = repo.find_commit(id)?;
-            if repo
-                .find_branch(branch_name, git2::BranchType::Local)?
-                .is_head()
-            {
-                repo.reset(final_commit.as_object(), git2::ResetType::Hard, None)?;
-            } else {
-                repo.branch(&branch_name, &final_commit, true)?;
-            }
-        }
-        _ => {}
+    let final_commit = repo.find_commit(final_id)?;
+    if repo
+        .find_branch(branch_name, git2::BranchType::Local)?
+        .is_head()
+    {
+        repo.reset(final_commit.as_object(), git2::ResetType::Hard, None)?;
+    } else {
+        repo.branch(&branch_name, &final_commit, true)?;
     }
 
     Ok(())
 }
 
 pub fn sync_command(ctx: &Ctx, args: &SyncArgs) -> Result<(), Error> {
+    save_command(
+        ctx,
+        &SaveArgs {
+            message: vec![
+                TEMP_COMMIT_PREFIX.to_string(),
+                "Save before sync".to_owned(),
+            ],
+        },
+        true,
+    )?;
+
     if args.names.len() == 0 {
         let repo_head = ctx.repo.head()?;
         let head_name_str = repo_head.name().unwrap();
         let head_name = head_name_str[head_name_str.rfind("/").map_or(0, |e| e + 1)..].to_owned();
-        return sync_branch(&ctx.repo, &head_name);
+        sync_branch(&ctx.repo, &head_name)?;
     }
     for branch in &args.names {
         sync_branch(&ctx.repo, &branch)?;
     }
 
-    Ok(())
+    pop_and_reset(ctx)
 }
