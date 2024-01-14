526b5625ff11abf3e91ea8b475b8d3c372ec5e25
Christoph Bartschat <christoph@barts.chat>
2024-01-14 14:22:59 -0800

    test

--- src/remote.rs
+++ src/remote.rs
@@ -33,7 +33,7 @@ pub fn sync_remote(ctx: &Ctx) -> Result<(), Error> {
             Cred::ssh_key(
                 username_from_url.unwrap(),
                 None,
-                std::path::Path::new(&format!("{}/.ssh/id_rsa", env::var("HOME").unwrap())),
+                std::path::Path::new(&format!("{}/.ssh/id_ed25519", env::var("HOME").unwrap())),
                 None,
             )
         });
