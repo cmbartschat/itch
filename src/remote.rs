7a955d4d68d92a97e1ec3ef4e9a725a2208f4008
Christoph Bartschat <christoph@barts.chat>
2024-05-26 19:56:55 +0200

    fix remote sync

--- src/remote.rs
+++ src/remote.rs
@@ -94,15 +94,41 @@ pub fn push_branch(ctx: &Ctx, branch: &str) -> Result<(), Error> {
 }
 
 pub fn pull_main(ctx: &Ctx) -> Result<(), Error> {
-    let remote = get_remote(ctx)?;
-    if let Some(mut remote) = remote {
-        remote.fetch(
-            &["main"],
-            Some(&mut setup_fetch_options(ctx)),
-            Some("Fetch main"),
-        )?;
+    match get_remote(ctx)? {
+        None => Ok(()),
+        Some(mut remote) => {
+            remote.fetch(
+                &["main"],
+                Some(&mut setup_fetch_options(ctx)),
+                Some("Fetch main"),
+            )?;
+
+            let local_main = ctx.repo.find_branch("main", git2::BranchType::Local)?;
+
+            let mut local_ref = ctx
+                .repo
+                .find_branch("main", git2::BranchType::Local)?
+                .into_reference();
+
+            let remote_commit = ctx
+                .repo
+                .reference_to_annotated_commit(&local_main.upstream()?.into_reference())?;
+
+            let analysis = ctx
+                .repo
+                .merge_analysis_for_ref(&local_ref, &[&remote_commit])?
+                .0;
+
+            if analysis.is_up_to_date() {
+                Ok(())
+            } else if analysis.is_fast_forward() {
+                local_ref.set_target(remote_commit.id(), "Sync main")?;
+                Ok(())
+            } else {
+                Err(Error::from_str("Local diverges from remote."))
+            }
+        }
     }
-    Ok(())
 }
 
 pub fn push_main(ctx: &Ctx) -> Result<(), Error> {
