9f6f7137c8f99ff4e0af794146ba16d180521e94
Christoph Bartschat <cmbartschat@gmail.com>
2024-10-21 09:10:56 -0700

    Save

--- src/path.rs
+++ src/path.rs
@@ -10,6 +10,7 @@ pub fn bytes2path(bytes: &[u8]) -> Maybe<PathBuf> {
     }
     #[cfg(windows)]
     {
+// windows!
         use std::str;
         match str::from_utf8(bytes) {
             Ok(s) => Ok(PathBuf::from(s)),
