8415889f408266cafbc84862872ffdf14868fc2e
Christoph Bartschat <christoph@barts.chat>
2024-10-23 19:39:49 -0700

    properly scope imports

--- src/path.rs
+++ src/path.rs
@@ -1,15 +1,17 @@
 use std::path::PathBuf;
 
-use crate::error::{fail, Maybe};
+use crate::error::Maybe;
 
 pub fn bytes2path(bytes: &[u8]) -> Maybe<PathBuf> {
     #[cfg(unix)]
     {
+        use std::ffi::OsStr;
         use std::os::unix::prelude::*;
         Ok(PathBuf::from(OsStr::from_bytes(bytes)))
     }
     #[cfg(windows)]
     {
+        use crate::error::{fail, Maybe};
         use std::str;
         match str::from_utf8(bytes) {
             Ok(s) => Ok(PathBuf::from(s)),
