ded768aecc0af22cd8b6cfcc0a3984c78b735b64
Christoph Bartschat <christoph@barts.chat>
2024-12-14 15:28:38 -0800

    tweak unsave behavior

--- src/command/unsave.rs
+++ src/command/unsave.rs
@@ -2,7 +2,11 @@ use std::path::Path;
 
 use git2::{build::TreeUpdateBuilder, Commit, FileMode, ResetType, Tree, TreeEntry};
 
-use crate::{cli::UnsaveArgs, ctx::Ctx, error::Attempt};
+use crate::{
+    cli::UnsaveArgs,
+    ctx::Ctx,
+    error::{fail, Attempt},
+};
 
 const GIT_FILEMODE_UNREADABLE: i32 = 0o000000;
 const GIT_FILEMODE_TREE: i32 = 0o040000;
@@ -38,6 +42,74 @@ fn get_entry_mode(entry: &TreeEntry) -> FileMode {
     FileMode::Unreadable
 }
 
+fn unsave_files(
+    ctx: &Ctx,
+    files: &[String],
+    head_commit: &Commit,
+    prev_commit: &Commit,
+) -> Attempt {
+    let current_tree = head_commit.tree()?;
+    let prev_tree = prev_commit.tree()?;
+
+    let mut new_tree_builder = TreeUpdateBuilder::new();
+
+    for file_path in files.iter().map(Path::new) {
+        match prev_tree.get_path(file_path) {
+            Ok(entry) => {
+                new_tree_builder.upsert(file_path, entry.id(), get_entry_mode(&entry));
+            }
+            Err(e) => match e.code() {
+                git2::ErrorCode::NotFound => {
+                    let should_delete = match current_tree.get_path(file_path) {
+                        Ok(_) => true,
+                        Err(e) => {
+                            if e.code() == git2::ErrorCode::NotFound {
+                                false
+                            } else {
+                                return Err(e);
+                            }
+                        }
+                    };
+                    if should_delete {
+                        new_tree_builder.remove(file_path);
+                    }
+                }
+                _ => return Err(e),
+            },
+        }
+    }
+
+    let new_tree: Tree = ctx
+        .repo
+        .find_tree(new_tree_builder.create_updated(&ctx.repo, &current_tree)?)?;
+    let parents: Vec<Commit> = head_commit.parents().collect();
+    let parent_refs: Vec<&Commit> = parents.iter().collect();
+
+    let committed = ctx.repo.commit(
+        None,
+        &head_commit.author(),
+        &head_commit.committer(),
+        head_commit.message().unwrap_or(""),
+        &new_tree,
+        &parent_refs,
+    )?;
+
+    ctx.repo.reset(
+        &ctx.repo.find_object(committed, None)?,
+        ResetType::Mixed,
+        None,
+    )?;
+
+    Ok(())
+}
+
+fn unsave_single(ctx: &Ctx, prev_commit: Commit) -> Attempt {
+    ctx.repo
+        .reset(&prev_commit.into_object(), ResetType::Mixed, None)?;
+
+    Ok(())
+}
+
 pub fn unsave_command(ctx: &Ctx, args: &UnsaveArgs) -> Attempt {
     let head_commit = ctx.repo.head()?.peel_to_commit()?;
     let base_commit = ctx
@@ -50,65 +122,20 @@ pub fn unsave_command(ctx: &Ctx, args: &UnsaveArgs) -> Attempt {
         .repo
         .find_commit(ctx.repo.merge_base(base_commit.id(), head_commit.id())?)?;
 
-    if !args.args.is_empty() {
-        let current_tree = head_commit.tree()?;
-        let prev_commit = head_commit.parent(0)?;
-        let prev_tree = prev_commit.tree()?;
-
-        let mut new_tree_builder = TreeUpdateBuilder::new();
-
-        for file_path in args.args.iter().map(Path::new) {
-            match prev_tree.get_path(file_path) {
-                Ok(entry) => {
-                    new_tree_builder.upsert(file_path, entry.id(), get_entry_mode(&entry));
-                }
-                Err(e) => match e.code() {
-                    git2::ErrorCode::NotFound => {
-                        let should_delete = match current_tree.get_path(file_path) {
-                            Ok(_) => true,
-                            Err(e) => {
-                                if e.code() == git2::ErrorCode::NotFound {
-                                    false
-                                } else {
-                                    return Err(e);
-                                }
-                            }
-                        };
-                        if should_delete {
-                            new_tree_builder.remove(file_path);
-                        }
-                    }
-                    _ => return Err(e),
-                },
-            }
-        }
-
-        let new_tree: Tree = ctx
-            .repo
-            .find_tree(new_tree_builder.create_updated(&ctx.repo, &current_tree)?)?;
-        let parents: Vec<Commit> = head_commit.parents().collect();
-        let parent_refs: Vec<&Commit> = parents.iter().collect();
-
-        let committed = ctx.repo.commit(
-            None,
-            &head_commit.author(),
-            &head_commit.committer(),
-            head_commit.message().unwrap_or(""),
-            &new_tree,
-            &parent_refs,
-        )?;
-
-        ctx.repo.reset(
-            &ctx.repo.find_object(committed, None)?,
-            ResetType::Mixed,
-            None,
-        )?;
-
-        return Ok(());
+    if head_commit.id() == fork_commit.id() {
+        return fail("Cannot unsave past the fork point");
     }
 
-    ctx.repo
-        .reset(&fork_commit.into_object(), ResetType::Mixed, None)?;
-
-    Ok(())
+    let mut parent_commits = head_commit.parents();
+    match (parent_commits.next(), parent_commits.next()) {
+        (None, _) => fail("Latest commit has no parent."),
+        (Some(prev_commit), None) => {
+            if !args.args.is_empty() {
+                unsave_files(ctx, &args.args, &head_commit, &prev_commit)
+            } else {
+                unsave_single(ctx, prev_commit)
+            }
+        }
+        (Some(_), Some(_)) => fail("Expected single parent commit."),
+    }
 }
