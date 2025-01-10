ccc395d5304f3c9838ea9122006d9fb12fdcc5c1
Christoph Bartschat <christoph@barts.chat>
2025-01-09 20:28:08 -0800

    📌 Save before switching to pager3

--- src/output.rs
+++ src/output.rs
@@ -1,12 +1,24 @@
 use std::{
     env,
     fmt::{self},
+    io::Stdout,
+};
+
+use tui::{
+    backend::CrosstermBackend,
+    widgets::{Block, Borders},
+    Terminal,
 };
 
 use crate::error::Maybe;
 
+pub struct PagerState {
+    terminal: Terminal<CrosstermBackend<Stdout>>,
+    lines: Vec<String>,
+}
+
 pub enum OutputTarget {
-    Pager(minus::Pager),
+    Pager(PagerState),
     Stdout,
 }
 
@@ -15,13 +27,21 @@ impl OutputTarget {
         if env::var_os("NOPAGER").is_some() {
             Ok(OutputTarget::Stdout)
         } else {
-            Ok(OutputTarget::Pager(minus::Pager::new()))
+            let stdout = std::io::stdout();
+            let backend = CrosstermBackend::new(stdout);
+            let terminal = Terminal::new(backend).expect("Terminal creation");
+            Ok(OutputTarget::Pager(PagerState {
+                terminal,
+                lines: vec![],
+            }))
         }
     }
 
     pub fn finish(self) {
         match self {
-            Self::Pager(p) => minus::page_all(p).unwrap(),
+            Self::Pager(p) => {
+                // p.terminal.clear();
+            }
             Self::Stdout => {}
         }
     }
@@ -30,7 +50,19 @@ impl OutputTarget {
 impl fmt::Write for OutputTarget {
     fn write_str(&mut self, s: &str) -> std::fmt::Result {
         match self {
-            Self::Pager(p) => p.write_str(s),
+            Self::Pager(ref mut p) => {
+                p.lines.push(s.into());
+
+                p.terminal.clear().expect("clear");
+                p.terminal
+                    .draw(|f| {
+                        let size = f.size();
+                        let block = Block::default().title("Block");
+                        f.render_widget(block, size);
+                    })
+                    .expect("Redraw");
+                Ok(())
+            }
             Self::Stdout => Ok(print!("{s}")),
         }
     }
