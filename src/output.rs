1f0093368c7b83a44981290300bd085ec76d0806
Christoph Bartschat <christoph@barts.chat>
2025-01-09 20:04:06 -0800

    📌 Save before switching to pager2

--- src/output.rs
+++ src/output.rs
@@ -3,34 +3,51 @@ use std::{
     fmt::{self},
 };
 
+use pager_rs::{CommandList, State, StatusBar};
+
 use crate::error::Maybe;
 
-pub enum OutputTarget {
-    Pager(minus::Pager),
+pub enum OutputTarget<'a> {
+    Pager(State<'a>),
     Stdout,
 }
 
-impl OutputTarget {
+impl OutputTarget<'_> {
     pub fn new() -> Maybe<Self> {
         if env::var_os("NOPAGER").is_some() {
             Ok(OutputTarget::Stdout)
         } else {
-            Ok(OutputTarget::Pager(minus::Pager::new()))
+            let status_bar = StatusBar::new("".to_string());
+            let mut state =
+                State::<'_>::new("test".to_string(), status_bar, CommandList::default())
+                    .expect("Could not open output stream.");
+
+            state.show_line_numbers = false;
+
+            pager_rs::init().expect("Pager init");
+            pager_rs::run(&mut state).expect("Pager start");
+            Ok(OutputTarget::Pager(state))
         }
     }
 
     pub fn finish(self) {
         match self {
-            Self::Pager(p) => minus::page_all(p).unwrap(),
+            Self::Pager(p) => {
+                pager_rs::finish().expect("Finish");
+            }
             Self::Stdout => {}
         }
     }
 }
 
-impl fmt::Write for OutputTarget {
+impl fmt::Write for OutputTarget<'_> {
     fn write_str(&mut self, s: &str) -> std::fmt::Result {
         match self {
-            Self::Pager(p) => p.write_str(s),
+            Self::Pager(p) => {
+                p.content = format!("{}\n{}", p.content, s);
+                p.home();
+                Ok(())
+            }
             Self::Stdout => Ok(print!("{s}")),
         }
     }
