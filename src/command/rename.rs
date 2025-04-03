4140748e15519d887391eb5f81a1104d2d925d2c
Christoph Bartschat <christoph@barts.chat>
2025-04-02 18:39:30 -0700

    prevent main branch from getting renamed

--- src/command/rename.rs
+++ src/command/rename.rs
@@ -1,10 +1,16 @@
 use crate::{
-    branch::get_current_branch, cli::RenameArgs, ctx::Ctx, error::Attempt,
+    branch::get_current_branch,
+    cli::RenameArgs,
+    ctx::Ctx,
+    error::{Attempt, fail},
     remote::try_delete_remote_branch,
 };
 
 pub fn rename_command(ctx: &Ctx, args: &RenameArgs) -> Attempt {
     let old_name = get_current_branch(ctx)?;
+    if old_name == "main" {
+        return fail("Cannot rename the main branch");
+    }
     let ref_name = format!("refs/heads/{}", args.name);
     let message = format!("Renaming current branch to {}", args.name);
     ctx.repo.head()?.rename(&ref_name, false, &message)?;
